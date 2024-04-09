f130e4a8eb7d641b183518d267cdfaab425b4b7e
Ji__ho <cale.jiho@gmail.com>
2024-04-09 20:41:58 +0900

    🐛 Fix: Notice error
    Notice zustand to Storage

--- lib/zustand/noticeStore.ts
+++ lib/zustand/noticeStore.ts
@@ -1,26 +1,32 @@
 import { create } from "zustand";
 import { NoticeViewType } from "@/template/notice";
-
+import { persist } from "zustand/middleware";
 interface NoticeState extends NoticeViewType {
-    contents: string
-    timeStamp: Date
-    title: string
-    viewCount: number
-    setNoticeState: (notice:NoticeViewType) => void;
+  contents: string;
+  timeStamp: Date;
+  title: string;
+  viewCount: number;
+  setNoticeState: (notice: NoticeViewType) => void;
 }
 
-const useNoticeStore = create<NoticeState>((set) => ({
-    contents: "",
-    timeStamp: new Date(),
-    title: "",
-    viewCount: 0,
-   
-    setNoticeState : (notice: NoticeViewType) => 
-        set({contents: notice.contents,
-            timeStamp: notice.timeStamp,
-            title: notice.title,
-            viewCount:notice.viewCount})
-    
-  }));
-  
-  export default useNoticeStore;
+const useNoticeStore = create<any>(
+  persist(
+    (set) => ({
+      contents: "",
+      timeStamp: new Date(),
+      title: "",
+      viewCount: 0,
+
+      setNoticeState: (notice: NoticeViewType) =>
+        set({
+          contents: notice.contents,
+          timeStamp: notice.timeStamp,
+          title: notice.title,
+          viewCount: notice.viewCount,
+        }),
+    }),
+    { name: "notice-storage" },
+  ),
+);
+
+export default useNoticeStore;
