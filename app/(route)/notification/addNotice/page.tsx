7a0af28e42f66a31b1869167609fbc24ea9db39f
Ji__ho <cale.jiho@gmail.com>
2024-04-11 16:32:37 +0900

    🐛 Fix: Quill Editor by dynamic import!!!!!!!!
    Fxxk to Quill Editor I will make my self!!!

--- app/(route)/notification/addNotice/page.tsx
+++ app/(route)/notification/addNotice/page.tsx
@@ -1,22 +1,19 @@
 import React, { ReactNode } from "react";
-import dynamic from "next/dynamic";
 // editor
 import { QuillEditor } from "@/app/components/notification/manage/QuillEditor";
 // components
-// const QuillEditor = dynamic(
-//   () => import("../../../components/notification/manage/QuillEditor") as any,
-//   { ssr: false },
-// );
+
 const Page = async (): Promise<ReactNode> => {
-  const editor =
-    process.env.NODE_ENV === "development" ? (
-      <QuillEditor />
-    ) : (
-      typeof window !== "undefined" && <QuillEditor />
-    );
+  // const editor =
+  //   process.env.NODE_ENV === "development" ? (
+  //     <QuillEditor />
+  //   ) : (
+  //     typeof window !== "undefined" && <QuillEditor />
+  //   );
   return (
     <main className="relative flex h-screen min-h-screen w-full flex-col items-center justify-start">
-      {editor}
+      {/* {editor} */}
+      <QuillEditor />
     </main>
   );
 };
