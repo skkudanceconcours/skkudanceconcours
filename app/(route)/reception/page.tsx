a0d0d384b8c9e1c349ca6cb83455c7065b4c66f5
JungHwan <wjdghksdl00@gmail.com>
2024-04-12 18:00:11 +0900

    ✏️ Fix: typo
    reception -> registration

--- app/(route)/reception/page.tsx
+++ app/(route)/reception/page.tsx
@@ -5,7 +5,7 @@ const ReceptionPage = (): ReactNode => {
   return (
     <main className="relative flex min-h-screen w-screen flex-col px-8 lg:px-40 ">
       <h1 className="relative lg:my-12 flex h-[15vh] w-4/5 flex-col justify-center text-2xl lg:text-5xl font-semibold underline underline-offset-[15px]">
-         Reception
+      REGISTRATION
       </h1>
       <ReceptionForm />
     </main>
