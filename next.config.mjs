3de7517ede5feedff25f4318ab1edbc12ccfd308
JungHwan <wjdghksdl00@gmail.com>
2024-04-18 15:32:04 +0900

    🐛 Fix: CORS
    cors fix trial4

--- next.config.mjs
+++ next.config.mjs
@@ -10,23 +10,23 @@ const nextConfig = {
 
     return config;
   },
-  //   async headers() {
-  //     return [
-  //       {
-  //         source: "/api/:path*",
-  //         headers: [
-  //             { key: "Access-Control-Allow-Credentials", value: "true" },
-  //             { key: "Access-Control-Allow-Origin", value: "https://www.skkudanceconcours.kr/reception/admin" }, // replace this your actual origin
-  //             { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-  //             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-  //         ]
-  //       }
-  //     ]
-  // }
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "https://www.skkudanceconcours.kr/reception/admin" }, // replace this your actual origin
+              { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+  },
   async rewrites() {
     return [
       {
-        source: "/:path*",
+        source: "/api/:path*",
         destination: `https://skkudanceconcours.kr/:path*`,
       },
     ];
