94e3a98a6e994f6f4a36434adf11e1258ff35164
Ji__ho <cale.jiho@gmail.com>
2024-05-03 13:16:20 +0900

    💚 Fix: firebase
    firebase exchange

--- lib/firebase/firebaseConfig.ts
+++ lib/firebase/firebaseConfig.ts
@@ -3,26 +3,26 @@ import { getFirestore, collection } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 // Origin Firebase
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//   appId: process.env.NEXT_PUBLIC_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-// };
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
 
 // Test Firebase
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_TEST_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_TEST_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_TEST_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_TEST_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_TEST_AUTH_DOMAIN,
-  appId: process.env.NEXT_PUBLIC_TEST_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_TEST_MEASUREMENT_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_TEST_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_TEST_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_TEST_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_TEST_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_TEST_AUTH_DOMAIN,
+//   appId: process.env.NEXT_PUBLIC_TEST_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_TEST_MEASUREMENT_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
