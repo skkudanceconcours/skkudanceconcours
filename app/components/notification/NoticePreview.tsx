2b2162c2ac91664a5ae22e1421c8dee9c939e906
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-19 23:13:18 +0900

    📱 Feat(UI): Notice responsive design

--- app/components/notification/NoticePreview.tsx
+++ app/components/notification/NoticePreview.tsx
@@ -1,14 +1,15 @@
-"use client";
-import React, { ReactNode } from "react";
-import { useRouter } from "next/navigation";
-// type
-import { NoticeType, NoticeViewType } from "@/template/notice";
-// firebase
-import { updateViewCount } from "@/lib/firebase/firebaseCRUD";
-// context
-import useNoticeStore from "@/lib/zustand/noticeStore";
+import React, { ReactNode, useState } from 'react';
+import Link from 'next/link';
 
-const NoticePreview = ({
+// type
+import { NoticeType, NoticeViewType } from '@/template/notice';
+// firebase
+import { updateViewCount } from '@/lib/firebase/firebaseCRUD';
+// context
+import useNoticeStore from '@/lib/zustand/noticeStore';
+import { Path } from '@/template/paths';
+
+export default function NoticePreview({
   id,
   num,
   contents,
@@ -17,54 +18,44 @@ const NoticePreview = ({
   viewCount,
   important,
   files,
-}: NoticeType): ReactNode => {
+}: NoticeType): ReactNode {
   timeStamp = new Date(timeStamp);
-  // 선언
-  const router = useRouter();
+
+  // Models
   const { setNoticeState } = useNoticeStore();
-  const month: String = String(timeStamp.getMonth() + 1).padStart(2, "0");
-  const day: String = String(timeStamp.getDate()).padStart(2, "0"); // 일을 가져와서 두 자리 수로 만듭니다.
+  const month: String = String(timeStamp.getMonth() + 1).padStart(2, '0');
+  const day: String = String(timeStamp.getDate()).padStart(2, '0'); // 일을 가져와서 두 자리 수로 만듭니다.
   const dateString: string = `${month}-${day}`;
 
   // dynamic_styles
   const is_imp = important
-    ? "flex h-[5.5vh] w-full items-center font-bold border-b-1 border-solid border-[#e8e8e8] bg-[#fbfbfb]"
-    : "flex h-[5.5vh] w-full items-center border-b-1 border-solid border-[#e8e8e8] hover:bg-[#fbfbfb]";
+    ? ' flex h-fit sm:h-[7.5vh] w-full items-center font-bold border-b-1 border-solid border-[#e8e8e8] bg-[#fbfbfb]'
+    : ' flex h-fit sm:h-[7.5vh] w-full items-center border-b-1 border-solid border-[#e8e8e8] hover:bg-[#fbfbfb]';
 
   return (
     <div className={is_imp}>
-      <div className="flex w-[12%] items-center justify-center">
-        {important ? "공지" : num}
-      </div>
-      <div className="flex w-[64%] items-center justify-center  hover:cursor-pointer hover:underline">
-        <div
-          className="cursor-pointer"
-          onClick={() => {
-            const queryData: NoticeViewType = {
-              id,
-              contents,
-              timeStamp: timeStamp,
-              title,
-              viewCount: ++viewCount, // 백엔드에 업데이트 되기 전에 Client에서 미리 표시
-              files: files,
-            };
+      <div className='flex w-[12%] items-center justify-center'>{important ? '공지' : num}</div>
+      <Link
+        href={`notification/details` as Path}
+        className='flex h-full w-[64%] cursor-pointer items-center justify-center whitespace-normal px-1 py-2 hover:cursor-pointer hover:underline'
+        onClick={() => {
+          const queryData: NoticeViewType = {
+            id,
+            contents,
+            timeStamp: timeStamp,
+            title,
+            viewCount: ++viewCount, // 백엔드에 업데이트 되기 전에 Client에서 미리 표시
+            files: files,
+          };
 
-            updateViewCount(id as string);
-            setNoticeState(queryData);
-            router.push("/notification/details", { scroll: false });
-          }}
-        >
-          {title ? title : "(제목이 없습니다)"}
-        </div>
-      </div>
-      <div className="flex w-[12%] items-center justify-center">
-        {viewCount}
-      </div>
-      <div className="flex w-[12%] items-center justify-center">
-        {dateString}
-      </div>
+          updateViewCount(id as string);
+          setNoticeState(queryData);
+        }}
+      >
+        {title ? title : '(제목이 없습니다)'}
+      </Link>
+      <div className='flex w-[12%] items-center justify-center'>{viewCount}</div>
+      <div className='flex w-[12%] items-center justify-center'>{dateString}</div>
     </div>
   );
-};
-
-export default NoticePreview;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
-import { anton, raleway } from "@/public/fonts/font";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { anton, raleway } from '@/public/fonts/font';
+import Header from './components/Header';
+import Footer from './components/Footer';
 // context
-import { NextUIProviders } from "@/lib/provider/NextUiProviders";
-import LoginStateProvider from "@/lib/provider/LoginStateProvider";
+import { NextUIProviders } from '@/lib/provider/NextUiProviders';
+import LoginStateProvider from '@/lib/provider/LoginStateProvider';
 export const metadata: Metadata = {
-  title: "성균관대학교 무용학과 콩쿨",
-  description: "성균관대학교 무용학과 콩쿨 웹사이트",
+  title: '성균관대학교 무용학과 콩쿨',
+  description: '성균관대학교 무용학과 콩쿨 웹사이트',
 };
 
 export default function RootLayout({
@@ -18,15 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="relative" lang="en">
-      <body className={`${raleway.className} flex flex-col w-full overflow-x-hidden`}>
+    <html className='relative' lang='en'>
+      <body className={`${raleway.className} flex w-full flex-col overflow-x-hidden`}>
         <NextUIProviders>
           <LoginStateProvider>
             <Header />
             {children}
             <Footer />
           </LoginStateProvider>
-          <div id="modal-root"></div>
+          <div id='modal-root'></div>
         </NextUIProviders>
       </body>
     </html>
